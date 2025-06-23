06c4df151946f23faada8d7749b06745495337a2
minhera <120227761+minhera@users.noreply.github.com>
2025-06-23 16:11:37 -0700

    Update main.cpp - default argument

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
 //
 // Created by User on 6/23/2025.
 //
+//
+// Created by User on 6/23/2025.
+//
+#include <stdio.h>
+
+#include "seating.h"
+#include "log.h"
+
+unsigned int production_limit = 120;
+
+unsigned int consumer_tx = 0;
+unsigned int consumer_rev9 = 0;
+unsigned int producer_gt = 0;
+unsigned int producer_vip = 0;
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
