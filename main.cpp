4b5c7c4df7b45316bfeeca7df78df78ec317e858
Ryan Alakija <108511276+Ryan-Alakija@users.noreply.github.com>
2025-06-24 23:33:18 -0700

    Update main.cpp
    Changed variable names and added error handling for non-int command-line flag arguments (i.e. `./main -s hi`)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 // Names: Minh Tran & Ryan Alakija
-// Red ID: 
-//
-// Created by User on 6/23/2025.
-//
+// Red ID: <Minh Red ID> && 826620776
 //
 // Created by User on 6/23/2025.
 //
@@ -17,32 +14,26 @@
 #define EXIT_NORMAL 0       //normal exit flag
 #define EXIT_ERROR 1        //error exit flag
 
-//remove these global variables later, ONLY FOR REFERENCE
-//default values for command line arguments
-
+//some other functions to add (maybe)
 
 int main(int argc, char* argv[]) {
 
     /*** COMMAND LINE USER INTERFACE + INITIALIZATION ***/
     
     //declare & initialize variables w/ default values
-    unsigned int production_limit = 120;                //total number of seating requests
+    unsigned int total_seat_reqs = 120;             //total number of seating requests that will take place in the simulation
 
-    unsigned int consumer_tx = 0;                       //average time for T-X robot to process seating request + guide customers to proper seating
-    unsigned int consumer_rev9 = 0;                     //average time for Rev-9 robot (same as T-X robot)
-    unsigned int producer_gt = 0;                       //average time for general table greeter robot to produce and insert general table request
-    unsigned int producer_vip = 0;                      //average time for VIP room greeter robot to produce and insert VIP room request
-
-    /* some alternate variable names I am proposing
-     *    numSeats / num_seats;
-     *    txTime / tx_time;
-     *    rev9Time / rev9_time / rev_9_time;
-     *    tableGreeterTime / table_greeter_time;
-     *    vipTime / vip_time;
-     */
+    //consumers
+    unsigned int tx_time = 0;                       //average time for T-X robot to process seating request + guide customers to proper seating
+    unsigned int rev9_time = 0;                     //average time for Rev-9 robot (same as T-X robot)
+    
+    //producers
+    unsigned int general_time = 0;                  //average time for general table greeter robot to produce and insert general table request
+    unsigned int vip_time = 0;                      //average time for VIP room greeter robot to produce and insert VIP room request
 
     //declare other variables
-    int option;
+    int option;                 //an int variable that obtains the return value of getopt() function
+    int temp_int;               //a temporary int variable to store the converted int value of optarg
 
     /* process optional arguments; each of them take numerical arguments
      * note: getopt() uses unistd.h library, which also includes getopt.h
@@ -51,35 +42,57 @@ int main(int argc, char* argv[]) {
      *    optind - the index that represents the first argument in argv to process AFTER
      *             all the optional arguments are processed; we will not need this here
      */
+    
+    //if getopt() returns -1, there are no command-line flag arguments given, thus we skip
     while ((option = getopt(argc, argv, "s:x:r:g:v:")) != -1) {
+        //error handling for any non-int arguments;
+        //explicitly not required by the instructions, but implemented for good practice
+        try {
+            temp_int = std::stoi(optarg);
+
+            //check if argument is 0 or greater (although 0 would be redundant for most flags besides -s)
+            if (temp_int < 0) {
+                //we could throw an exception as a part of this try-catch block,
+                //but it's easier to just print an error statement here and exit
+                fprintf(stderr, "Please enter a non-negative number\n");
+                exit(EXIT_ERROR);
+            }
+        }
+        catch (...) {
+            fprintf(stderr, "Each flag argument must be a non-negative number\n");
+            exit(EXIT_ERROR);
+        }
+        
+        //now that we passed the error-detecting try-catch block, we will process these optional arguments
         switch (option) {
             case 's':
-                production_limit = std::stoi(optarg);
+                total_seat_reqs = temp_int;
                 break;
             case 'x':
-                consumer_tx = std::stoi(optarg);
+                tx_time = temp_int;
                 break;
             case 'r':
-                consumer_rev9 = std::stoi(optarg);
+                rev9_time = temp_int;
                 break;
             case 'g':
-                producer_gt = std::stoi(optarg);
+                general_time = temp_int;
                 break;
             case 'v':
-                producer_vip = std::stoi(optarg);
+                vip_time = temp_int;
                 break;
             default:
+                //for if the user enters a flag with no argument after it
                 fprintf(stderr, "usage: %s [ -s | -x | -r | -g | -v ]\n", argv[0]);
                 exit(EXIT_ERROR);
         }
     }
 
     //TESTING: Command-line user interface
-    printf("Total number of seats: %d\n", production_limit);
-    printf("T-X robot average time: %d\n", consumer_tx);
-    printf("Rev-9 robot average time: %d\n", consumer_rev9);
-    printf("General table greeter average time: %d\n", producer_gt);
-    printf("VIP room greeter average time: %d\n", producer_vip);
+    printf("Total number of seats: %d\n", total_seat_reqs);
+    printf("T-X robot average time: %d\n", tx_time);
+    printf("Rev-9 robot average time: %d\n", rev9_time);
+    printf("General table greeter average time: %d\n", general_time);
+    printf("VIP room greeter average time: %d\n", vip_time);
 
     return 0;
 }
