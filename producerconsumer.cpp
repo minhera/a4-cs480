a1fcd9c68049cb5879aae0ee0ca7393b6da0b883
minhera <120227761+minhera@users.noreply.github.com>
2025-06-24 22:00:30 -0700

    Update producerconsumer.cpp

--- producerconsumer.cpp
+++ producerconsumer.cpp
@@ -1,25 +1,25 @@
 #include "producerconsumer.h"
 
 void producer_general(int time){
-  int delay = time;
   pthread_mutex_lock( &mutex1 );
   queue_add(general);
   pthread_mutex_unlock( &mutex1 );
 
-  if (delay > 0){
-    usleep(delay)
+  if (time > 0){
+    usleep(time);
   }
+  return NULL;
 }
 
 void producer_vip(int time){
-  int delay = time;
   pthread_mutex_lock( &mutex1 );
   queue_add(vip);
   pthread_mutex_unlock( &mutex1 );
 
-    if (delay > 0){
-    usleep(delay)
+  if (time > 0){
+    usleep(time);
   }
+  return NULL;
 }
 
 void consumer_tx(int time){
@@ -28,18 +28,19 @@ void consumer_tx(int time){
   queue_remove(item);
   pthread_mutex_unlock( &mutex1 );
 
-    if (delay > 0){
-    usleep(delay)
+  if (time > 0){
+    usleep(time);
   }
+  return NULL;
 }
 
 void consumer_rev9(int time){
-  int delay = time;
   pthread_mutex_lock( &mutex1 );
   queue_remove(item);
   pthread_mutex_unlock( &mutex1 );
 
-    if (delay > 0){
-    usleep(delay)
+  if (time > 0){
+    usleep(time);
   }
+  return NULL;
 }
