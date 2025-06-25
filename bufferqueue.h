648a96792e1437e776caea3a2a531e17db5bee40
minhera <120227761+minhera@users.noreply.github.com>
2025-06-25 01:33:14 -0700

    Update bufferqueue.h

--- bufferqueue.h
+++ bufferqueue.h
@@ -1,1 +1,12 @@
+#ifndef QUEUE_H              
+#define QUEUE_H
 
+#include "seating.h"       
+
+pthread_mutex_t mutex1;  
+pthread_cond_t not_full;          
+pthread_cond_t not_empty; 
+
+void queue_init();                  
+void queue_add(RequestType table);   
+void queue_remove(ConsumerType machine);
