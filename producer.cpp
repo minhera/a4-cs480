53be600f621a8804830748a67ff4105dc84d687b
minhera <120227761+minhera@users.noreply.github.com>
2025-06-26 01:37:26 -0700

    Update producer.cpp

--- producer.cpp
+++ producer.cpp
@@ -1,1 +1,28 @@
+#include "producer.h"     
+#include "queue.h"           
+#include <unistd.h>                 // For usleep
+#include <pthread.h>                // POSIX threads
 
+void *consumer_tx(void* time){
+  int tx_time = *(int*)time; 
+  pthread_mutex_lock( &mutex1 );
+  queue_remove(item);
+  pthread_mutex_unlock( &mutex1 );
+
+  if (time > 0){
+    usleep(tx_time);
+  }
+  return NULL;
+}
+
+void *consumer_rev9(void* time){
+  int rev9_time = *(int*)time; 
+  pthread_mutex_lock( &mutex1 );
+  queue_remove(item);
+  pthread_mutex_unlock( &mutex1 );
+
+  if (time > 0){
+    usleep(rev9_time);
+  }
+  return NULL;
+}
