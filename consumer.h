445384764502e131cdb21a314aa13e924748b8e0
minhera <120227761+minhera@users.noreply.github.com>
2025-06-26 01:33:03 -0700

    Create consumer.h

---
+++ consumer.h
@@ -0,0 +1,7 @@
+#ifndef CONSUMER_H           
+#define CONSUMER_H
+
+void* consumer_tx(void* arg);        // Function to consume requests by T-X robot
+void* consumer_rev9(void* arg);      // Function to consume requests by Rev-9 robot
+
+#endif
