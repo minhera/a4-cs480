5279607abec33938fbb3c8b48e6371560addf2bd
Ryan Alakija <108511276+Ryan-Alakija@users.noreply.github.com>
2025-06-27 07:31:46 -0700

    Update producer.cpp
    Put a reminder to get rid of this global variable

--- producer.cpp
+++ producer.cpp
@@ -1,29 +1,45 @@
-#include "producer.h"     
-#include "queue.h"           
+#include "producer.h"
+#include "queue.h"
 #include <unistd.h>                 // For usleep
 #include <pthread.h>                // POSIX threads
+#include <semaphore.h>
 
-void* producer_general(void* time){
-  int general_time = *(int*)time; 
-  while (true){ // Makes sure this thread run indefinitely
-    pthread_mutex_lock( &mutex1 );
-    queue_add(item);
-    pthread_mutex_unlock( &mutex1 );
-  
-    usleep(general_time);
-  }
-  return nullptr;
+//GET RID OF THIS GLOBAL VARIABLE, PUT IT SOMEWHERE ELSE
+extern sem_t barrier_sem;
+
+// Producer thread for general table requests
+void* producer_general(void* arg) {
+    int delay = *((int*)arg);       // Delay in milliseconds
+    while (true) {
+        pthread_mutex_lock(&queue_lock);                                 // Lock queue for safe access
+        if (produced[GeneralTable] + produced[VIPRoom] >= production_limit) {
+            pthread_mutex_unlock(&queue_lock);                           // Unlock and exit if limit reached
+            break;
+        }
+        queue_add(GeneralTable);                                          // Add general table request
+        pthread_mutex_unlock(&queue_lock);                               // Unlock after adding
+        if (delay > 0) usleep(delay * 1000);                              // Simulate production delay
+    }
+    return nullptr;
 }
 
-void *producer_vip(void* time){
-  int vip_time = *(int*)time; 
-  while (true){ // Makes sure this thread run indefinitely
-    pthread_mutex_lock( &mutex1 );
-    queue_add(item);
-    pthread_mutex_unlock( &mutex1 );
-  
-  
-    usleep(vip_time);
-  }
-  return nullptr;
+// Producer thread for VIP room requests
+void* producer_vip(void* arg) {
+    int delay = *((int*)arg);
+    while (true) {
+        pthread_mutex_lock(&queue_lock);                                 // Lock queue
+        if (produced[GeneralTable] + produced[VIPRoom] >= production_limit) {
+            pthread_mutex_unlock(&queue_lock);                           // Exit if production limit reached
+            break;
+        }
+        if (inRequestQueue[VIPRoom] >= 5) {                               // Respect VIP room max constraint
+            pthread_mutex_unlock(&queue_lock);                           // Unlock and retry
+            usleep(delay * 1000);
+            continue;
+        }
+        queue_add(VIPRoom);                                              // Add VIP request
+        pthread_mutex_unlock(&queue_lock);                               // Unlock
+        if (delay > 0) usleep(delay * 1000);                              // Simulate delay
+    }
+    return nullptr;
 }
