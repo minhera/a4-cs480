a52ba9ab3cd6a59a6ec999424d1864b887184234
minhera <120227761+minhera@users.noreply.github.com>
2025-06-26 21:41:22 -0700

    Update producer.cpp

--- producer.cpp
+++ producer.cpp
@@ -5,10 +5,11 @@
 
 void* producer_general(void* time){
   int general_time = *(int*)time; 
-  pthread_mutex_lock( &mutex1 );
-  queue_remove(item);
-  pthread_mutex_unlock( &mutex1 );
-
+  while (true){ // Makes sure this thread run indefinitely
+    pthread_mutex_lock( &mutex1 );
+    queue_remove(item);
+    pthread_mutex_unlock( &mutex1 );
+  }
   if (general_time > 0){
     usleep(general_time);
   }
@@ -17,10 +18,11 @@ void* producer_general(void* time){
 
 void *consumer_rev9(void* time){
   int rev9_time = *(int*)time; 
-  pthread_mutex_lock( &mutex1 );
-  queue_remove(item);
-  pthread_mutex_unlock( &mutex1 );
-
+  while (true){ // Makes sure this thread run indefinitely
+    pthread_mutex_lock( &mutex1 );
+    queue_remove(item);
+    pthread_mutex_unlock( &mutex1 );
+  }
   if (rev9_time > 0){
     usleep(rev9_time);
   }
