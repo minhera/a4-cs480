29d1a918bbdf78b5e43d055d40f6a4e8de35c8be
Ryan Alakija <108511276+Ryan-Alakija@users.noreply.github.com>
2025-06-27 17:46:26 -0700

    Update consumer.cpp
    Comments + Name & Red ID

--- consumer.cpp
+++ consumer.cpp
@@ -1,40 +1,60 @@
+// Names: Minh Tran & Ryan Alakija
+// Red ID: 129986763 && 826620776
+
 #include "consumer.h"
 #include "queue.h"
 #include <unistd.h>                 // For usleep
 #include <pthread.h>                // POSIX threads
 #include <semaphore.h>
 
-//GET RID OF THIS GLOBAL VARIABLE
-
+int count = 0;
 extern sem_t barrier_sem;
+
+/* General note
+ * The delay time is being measured in milliseconds, and usleep is in microseconds
+ * So we will multiply by 1000 (10^3) => (10^-6) * (10^3) = 10^-3 (milliseconds)
+ */
 
 // Consumer thread for T-X robot
 void* consumer_tx(void* arg) {
-    int delay = *((int*)arg);
+    //how long the request takes
+    int delay = *((int*)arg);       //cast the argument to int* and dereference it to obtain the value
+    
+    //run while loop indefinitely until there are no more requests left
     while (true) {
         pthread_mutex_lock(&queue_lock);
         queue_remove(TX);
-        if (produced[GeneralTable] + produced[VIPRoom] == production_limit && inRequestQueue[GeneralTable] + inRequestQueue[VIPRoom] == 0)
-            sem_post(&barrier_sem);
-        pthread_mutex_unlock(&queue_lock);                            // Remove request from queue
-        if (delay > 0) usleep(delay * 1000);                              // Simulate consumption delay
+        
+        //check if the total requests produced has reached the number of seat requests
+        //AND that there are no more requests in the queue
+        if (produced[GeneralTable] + produced[VIPRoom] >= total_seat_reqs && inRequestQueue[GeneralTable] + inRequestQueue[VIPRoom] == 0)
+            sem_post(&barrier_sem);             //once that happens, we can signal the main thread that we are done
+        
+        pthread_mutex_unlock(&queue_lock);                              // Remove request from queue
+
+        // Simulate consumption delay in milliseconds
+        if (delay > 0)
+            usleep(delay * 1000);
 
     }
-    return NULL;
+    return nullptr;                 //terminate the thread
 }
 
 // Consumer thread for Rev-9 robot
 void* consumer_rev9(void* arg) {
     int delay = *((int*)arg);
+    
+    //run while loop indefinitely
     while (true) {
         pthread_mutex_lock(&queue_lock);
         queue_remove(Rev9);
-        if (produced[GeneralTable] + produced[VIPRoom] == production_limit && inRequestQueue[GeneralTable] + inRequestQueue[VIPRoom] == 0)
+        if (produced[GeneralTable] + produced[VIPRoom] >= total_seat_reqs && inRequestQueue[GeneralTable] + inRequestQueue[VIPRoom] == 0)
             sem_post(&barrier_sem);
         pthread_mutex_unlock(&queue_lock);              // Remove request from queue
+        // Simulate consumption delay in milliseconds
         if (delay > 0)
-            usleep(delay * 1000);                           // Simulate delay
+            usleep(delay * 1000);
 
     }
-    return NULL;
+    return nullptr;             //terminate thread
 }
