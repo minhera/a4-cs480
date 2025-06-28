0ae952784f31e3aaaaa27703631b639fa26400ec
minhera <120227761+minhera@users.noreply.github.com>
2025-06-27 18:38:21 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -15,7 +15,7 @@ unsigned int produced[];                    //array to keep track of number of e
 unsigned int inRequestQueue[];              //array to keep track of number of each type currently in the queue
 unsigned int* consumed[];                   //array to keep track of number of each type that has been consumed
 unsigned int total_seat_reqs;               //the total number of seat requests
-pthread_mutex_t queue_lock;                 //mutex variable to lock the queue and keep it mutually exclusive
+pthread_mutex_t mutex1;                 //mutex variable to lock the queue and keep it mutually exclusive
 pthread_cond_t not_full;                    //checks if the queue is no longer full
 pthread_cond_t not_empty;                   //checks if the queue is no longer empty
 
